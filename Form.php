72419143b24fd1fa52a590ebdf8084a75caf1efb
LilSaint <pamela.carric@gmail.com>
2016-12-29 18:39:58 +0100

    Apprentissage form

--- Form.php
+++ Form.php
@@ -5,24 +5,44 @@ class Form {
     private $data;
     public $paragraphe = 'p';
 
-    public function __construct($data) {
+    public function __construct($data = []) {
         $this->data = $data;
     }
 
     // fonction privée qui prend en paramètre le contenu html, et qui n'est dispo que dans ma classe
     private function surround($html) {
         // Les {} sont nécessaires pour que le code soit interprété par les doubles ""
-        return "<{$this->surround}>$html</{$this->surround}>";
-        
+        return "<{$this->paragraphe}>{$html}</{$this->paragraphe}>";
+
+    }
+
+    // """***""" GETTEURS """***"""
+
+    private function getValue($index) {
+        if(isset($this->data[$index])) {
+            return $this->data[$index];
+        } else {
+            return NULL;
+        }
+
+        // Ternaire
+        // return isset($this->data[$index]) ? $this->data[$index] : NULL;
     }
 
     public function input($name) {
-        return $this->surround('<input type="text" name="' . $name . '">');
+        return $this->surround(
+            '<input type="text" name="' . $name . '" value="' . $this->getValue($name) . '">'
+        );
     }
 
     public function submit() {
         return $this->surround('<button type="submit">Envoyer</button>');
     }
 }
+
+// private function getValue($index) {
+//        return $this->data[$index];
+//    }
+// --- $this->data = contient toutes les données passé en param de mon construct
 
 ?>
