0e448c380d8fc0664115a870da1806295a720e4c
LilSaint <pamela.carric@gmail.com>
2016-12-30 19:29:04 +0100

    1er utilisation de PDO pour la connection à la BDD

--- pages/home.php
+++ pages/home.php
@@ -1,6 +1,17 @@
-<h1>
-    Je suis la homepage
-</h1>
-<div class="container">
-    <a href="index.php?p=single">Page Single</a>
-</div>
+<?php
+
+$connectBDD = new PDO('mysql:dbname=test_blog;host=localhost', 'root', '');
+
+$connectBDD->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$req = "INSERT INTO billet SET titre_post='Mon titre', date='" . date('Y-m-d H:i:s') . "'";
+
+$count_row = $connectBDD->exec($req);
+
+var_dump($count_row);
+
+?>
+
+<!-- **************************** -->
+<!-- *** $connectBDD->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); *** -->
+<!-- Permet de debug PDO et renvoie les erreurs et les exceptions concernant la requête -->
