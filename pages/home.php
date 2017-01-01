81c2834cd1adc8e52cc4156905e6c7af3a5a876b
LilSaint <pamela.carric@gmail.com>
2017-01-01 10:33:33 +0100

    Affichage des catégories

--- pages/home.php
+++ pages/home.php
@@ -1,13 +1,24 @@
 <!-- Page Home -->
-<?php
-$req = "SELECT * FROM billet";
-$reponse = App\Appli::getDatabase()->query($req, 'App\Tables\Articles');
-foreach($reponse as $post):?>
+<div class="row">
+    <div class="col-sm-8">
+        <?php
+        $reponse = \App\Tables\Articles::getLast();
+        foreach($reponse as $post):?>
 
-<a href="<?= $post->url ?>"><h2><?= $post->titre_post; ?></h2></a>
+        <a href="<?= $post->url ?>"><h2><?= $post->titre_post; ?></h2></a>
+        <span><?= $post->titre_cat ?></span>
 
-<p>
-    <?= $post->getExtrait(); ?>
-</p>
+        <p>
+            <?= $post->getExtrait(); ?>
+        </p>
 
-<?php endforeach; ?>
+        <?php endforeach; ?>
+    </div>
+    <div class="col-sm-4">
+        <ul>
+            <?php foreach(App\Tables\Categories::all() as $categories): ?>
+            <li><a href="<?= $categories->url; ?>"></a><?= $categories->titre_cat; ?></li>
+            <?php endforeach; ?>
+        </ul>
+    </div>
+</div>
