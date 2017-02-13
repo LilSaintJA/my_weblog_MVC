025f5c8f104287a6147fbedd8cc44d53c36c87eb
LilSaint <pamela.carric@gmail.com>
2017-02-13 13:26:00 +0100

    Page qui require les views et le template

--- public/index.php
+++ public/index.php
@@ -2,31 +2,22 @@
 <?php
 define('ROOT', dirname(__DIR__));
 require_once(ROOT . '/app/Appli.php');
+
 // Permet de charger l'autoloader depuis Appli
 Appli::load();
 
 if(isset($_GET['p'])) {
     $p = $_GET['p'];
 } else {
-    $p = 'home';
+    $p = 'articles.index';
 }
-//
-ob_start();
-if($p === 'home') {
-    require ROOT . '/pages/articles/home.php';
-} elseif($p === 'article.show') {
-    require ROOT . '/pages/articles/article.php';
-} elseif ($p === 'article.categorie') {
-    require ROOT . '/pages/articles/categorie.php';
-} elseif ($p === 'login') {
-    require ROOT . '/pages/users/login.php';
-} elseif ($p === '404') {
-    require ROOT . '/pages/page404.php';
-}
-//
-//// Permet de stocker les require dans la variable $content
-$content = ob_get_clean();
-require ROOT . '/pages/templates/template.php';
+// Permet de séparer l'url, d'un côté le controller de l'autre la view
+$p = explode('.', $p);
+
+$action = $p[1];
+$controller = '\App\Controller\\' . ucfirst($p[0]) . 'Controller';
+$controller = new $controller();
+$controller->$action();
 
 ?>
 
